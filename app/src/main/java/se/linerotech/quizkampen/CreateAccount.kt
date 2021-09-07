7698a317d301a03f6d11721be7fe3c98ef02fa4a
Musadrammeh <43375006+Musadrammeh@users.noreply.github.com>
2021-09-07 12:11:50 +0200

    feat: Create Account
    Creation of account with password and e-mail.
    
    Creating variables for viewbinding

--- app/src/main/java/se/linerotech/quizkampen/CreateAccount.kt
+++ app/src/main/java/se/linerotech/quizkampen/CreateAccount.kt
@@ -2,10 +2,20 @@ package se.linerotech.quizkampen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class CreateAccount : AppCompatActivity() {
+    private var mySignUpButton: Button? = null
+    private var myPassword: EditText? = null
+    private var myEmail: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        mySignUpButton = findViewById(R.id.buttonSignUp)
+        myPassword = findViewById(R.id.editTextSignUpPassword)
+        myEmail = findViewById(R.id.editTextSignUpEmail)
+
     }
 }
