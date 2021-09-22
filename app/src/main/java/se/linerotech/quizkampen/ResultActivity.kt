f3df83c528d31745ec9d1c53694a6e215ca9e51f
Musadrammeh <43375006+Musadrammeh@users.noreply.github.com>
2021-09-22 18:43:17 +0200

    feat: resultactivity_playAgain_quitGame
    Press the buttons to play again or to guit playing and go back to profilepage

--- app/src/main/java/se/linerotech/quizkampen/ResultActivity.kt
+++ app/src/main/java/se/linerotech/quizkampen/ResultActivity.kt
@@ -1,8 +1,17 @@
 package se.linerotech.quizkampen
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myquizgame.Backends.GetQuestions
+import com.example.myquizgame.RetrofitClient
+import com.example.myquizgame.models.Qustions
+import com.example.myquizgame.models.Result
+import com.example.myquizgame.models.Token
+import kotlinx.android.synthetic.main.activity_profile_page.*
+import retrofit2.Call
+import retrofit2.Callback
 import se.linerotech.quizkampen.GameActivity.Companion.SCORE
 
 
@@ -11,13 +20,75 @@ import se.linerotech.quizkampen.databinding.ActivityResultBinding
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityResultBinding.inflate(layoutInflater)
+        binding = ActivityResultBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val myScore=intent.getIntExtra(SCORE,0)
-        binding.score.text="Score $myScore/10"
+
+        val myScore = intent.getIntExtra(SCORE, 0)
+        binding.score.text = "Score $myScore/10"
 
 
+        binding.buttonPlay.setOnClickListener {
+            getMyToken()
+
+
+        }
+        buttonLogOut.setOnClickListener {
+            val bIntent = Intent(this, ProfilePage::class.java)
+            startActivity(bIntent)
+
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    private fun getMyToken() {
+        RetrofitClient
+            .instance
+            .getToken("request")
+            .enqueue(object : Callback<Token> {
+                override fun onResponse(call: Call<Token>, response: retrofit2.Response<Token>) {
+                    if (response.isSuccessful) {
+
+                        getQuestions(response.body()!!.token)
+                    }
+                }
+
+                override fun onFailure(call: Call<Token>, t: Throwable) {
+                }
+
+            })
+    }
+
+    private fun getQuestions(myToken: String) {
+
+        GetQuestions
+            .instance
+            .getQuestions("10", myToken)
+            .enqueue(object : Callback<Qustions> {
+                override fun onResponse(
+                    call: Call<Qustions>,
+                    response: retrofit2.Response<Qustions>
+                ) {
+                    if (response.isSuccessful) {
+
+                        val listOfRepos = response.body()?.results as? ArrayList<Result>
+                        listOfRepos?.let {
+                            val intent = Intent(this@ResultActivity, GameActivity::class.java)
+                            intent.putParcelableArrayListExtra(GameActivity.QUIZ_DATA, it)
+                            startActivity(intent)
+                        }
+                        
+                    }
+                }
+
+                override fun onFailure(call: Call<Qustions>, t: Throwable) {
+
+                }
+            })
     }
 }
